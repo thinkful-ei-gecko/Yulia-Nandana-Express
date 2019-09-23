e41671a3e650009c40876998b697ba92319e2384
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-09-23 12:33:59 -0700

    WIP cipher drill

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+'use strict';
 const express = require('express');
 const morgan = require('morgan');
 
@@ -11,6 +12,26 @@ app.get('/sum',(req,res)=>{
   const b = parseInt(req.query.b);
 
   res.send(`The sum of a and b is ${a+b}`);
+});
+
+app.get('/cipher',(req,res)=>{
+  const text = req.query.text;
+  const shift = req.query.shift;
+  let decipher = '';
+
+  function isUpperCase(str) {
+    return str === str.toUpperCase();
+  }
+
+  for(let i = 0; i < text.length; i++){
+    if(isUpperCase(text[i])){
+      decipher += String.fromCharCode((text.charCodeAt(i) + shift - 65) % 26 + 65);
+    } else {
+    //else add lowercase letters
+      decipher += String.fromCharCode((text.charCodeAt(i) + shift - 97) % 26 + 97);
+    }
+  }
+  res.send(decipher);
 });
 
 app.listen(8000,() => console.log('server running on 8000'));
